3d16d118f85c6b54366a4239b565a2f1dcbbb106
james bishop <s00187350@mail.itsligo.ie>
2020-01-21 12:53:06 +0000

    created the band class

---
+++ lab 1/Band.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    public abstract class Band
+    {
+        #region props
+
+        public string Name { get; set; }
+
+        public int Formed { get; set; }
+        public string Members {get; set; }
+
+
+        #endregion props
+
+        #region Constructor
+        public Band(string name, int formed, string members )
+        {
+            Name = name;
+            Formed = formed;
+            Members = members;
+        }
+
+
+
+
+        #endregion Constructor
+
+
+    }
+    //public class RockBand : Band
+    //{
+
+    //}
+}
